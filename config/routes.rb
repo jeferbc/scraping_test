3554b49463873bc988e8795019b11c3a27aa40bb
jeferbc <jeffe.bernal@gmail.com>
2017-08-08 13:04:22 -0500

    versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+
+  end
 end
